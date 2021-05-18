890f92219ab08106f20277650aba97718c651551
jwilson42 <wilson.jd022@gmail.com>
2021-05-17 18:42:40 -0700

    changed navbar to use react-bootstrap components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,73 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
 
-const Navbar = (props) => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <Link className="navbar-brand" to="/">MERN Auth</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="#navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarsExample07">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" exact to="/">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link"  to="/about">About</NavLink>
-                        </li>
-                    {   // Now we'll add in the part of the navbar for when a user is vs isn't logged in.
-                        props.isAuth ? 
-                        (
-                            <>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/profile">Profile</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <span className="nav-link logut-link" onClick={props.handleLogout}>Logout</span>
-                                </li>
-                            </>
-                        ) : 
-                        (
-                            <>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/signup">Signup</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to='/login'>Login</NavLink>
-                                </li>
-                            </>
-                        )
-                    }
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
-}
+import styled, { css } from 'styled-components';
 
-export default Navbar;
+// const Nav = styled.nav`
+//     background: ${({ theme }) => theme.body};
+//     color: ${({ theme }) => theme.text};
+// `
+
+
+const TopNavbar = (props) => {
+  return (
+    <Navbar bg={props.theme} variant={props.theme} expand="lg">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          MERN Auth
+        </Link>
+        <Navbar.Toggle aria-controls="topNav" />
+        <Navbar.Collapse id="topNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                About
+              </NavLink>
+            </li>
+            {
+              // Now we'll add in the part of the navbar for when a user is vs isn't logged in.
+              props.isAuth ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/profile">
+                      Profile
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <span
+                      className="nav-link logut-link"
+                      onClick={props.handleLogout}
+                    >
+                      Logout
+                    </span>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/signup">
+                      Signup
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/login">
+                      Login
+                    </NavLink>
+                  </li>
+                </>
+              )
+            }
+          </ul>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
+  );
+};
+
+export default TopNavbar;
